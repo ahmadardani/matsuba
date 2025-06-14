601d9a9b048f9dc2e0aef3d7815ac74ced3c6195
ahmadardani <ahmadardani2011@gmail.com>
2025-06-14 20:39:01 +0800

    update

--- matsuba.py
+++ matsuba.py
@@ -11,6 +11,15 @@ class MyApp(QMainWindow):
         # ✅ KDE global menu workaround for PyQt5 (if needed)
         self.setMenuBar(self.ui.menubar)
 
+        self.ui.btnMenuExtract.clicked.connect(self.go_to_tab_1)
+        self.ui.btnMenuGenerate.clicked.connect(self.go_to_tab_2)
+
+    def go_to_tab_1(self):
+        self.ui.tabWidget.setCurrentIndex(0)
+
+    def go_to_tab_2(self):
+        self.ui.tabWidget.setCurrentIndex(1)
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyApp()
